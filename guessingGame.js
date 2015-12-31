2764a0d317451a3fdbda24d9ea26f1ff3957cbc1
codyss <cody.schwarz@gmail.com>
2015-12-31 12:00:23 -0500

    JS merge fixes

--- guessingGame.js
+++ guessingGame.js
@@ -1,75 +1,78 @@
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
 
-var playersGuess,
-    winningNumber
 
+
+
+function Game() {
+    this.winningNumber = 0;
+    this.playersGuess = 0;
+    this.numberOfGuesses = 0;
+    this.guesses = [];
+}
+
+
+// Generate the Winning Number
+Game.prototype.winningNumberGenerator = function () {
+    this.winningNumber = Math.floor(Math.random() * 100);
+}
+
+// Fetch the Players Guess
+Game.prototype.playersGuessGenerator = function () {
+        var obj = document.getElementById('num-input');
+        this.playersGuess = parseInt(obj.value);
+        obj.value = '';
+}
 
 
 /* **** Guessing Game Functions **** */
 
-// Generate the Winning Number
-
-function generateWinningNumber(){
-    return Math.floor(Math.random() * 100);
-}
-
-winningNumber = generateWinningNumber();
-
-// Fetch the Players Guess
-
-function playersGuessSubmission(){
-    var obj = document.getElementById('num-input');
-    playersGuess = parseInt(obj.value);
-    obj.value = '';
-}
 
 // Determine if the next guess should be a lower or higher number
 
-function lowerOrHigher(){
-    if (playersGuess > winningNumber) {
-        return "higher";
+Game.prototype.lowerOrHigher = function () {
+    if(this.playersGuess > this.winningNumber) {
+        return 'higher';
     } else {
-        return "lower";
+        return 'lower';
     }
 }
 
-function guessMessage(){
-    if (Math.abs(playersGuess - winningNumber) > 20) {
-        return "Your guess is " + lowerOrHigher() + " and more than 20 digits from the winning number";
-    } else if (Math.abs(playersGuess - winningNumber) > 10) {
-        return "Your guess is " + lowerOrHigher() + " and within 20 digits of the winning number";
-    } else if (Math.abs(playersGuess - winningNumber) > 5) {
-        return "Your guess is " + lowerOrHigher() + " and within 10 digits of the winning number";
+
+//Generate message to display to player
+
+Game.prototype.guessMessage = function() {
+    if (Math.abs(this.playersGuess - this.winningNumber) > 20) {
+        return "Your guess is " + this.lowerOrHigher() + " and more than 20 digits from the winning number";
+    } else if (Math.abs(this.playersGuess - this.winningNumber) > 10) {
+        return "Your guess is " + this.lowerOrHigher() + " and within 20 digits of the winning number";
+    } else if (Math.abs(this.playersGuess - this.winningNumber) > 5) {
+        return "Your guess is " + this.lowerOrHigher() + " and within 10 digits of the winning number";
     } else {
-        return "Your guess is " + lowerOrHigher() + " and within 5 digits of the winning number";
-    }
+        return "Your guess is " + this.lowerOrHigher() + " and within 5 digits of the winning number";
+    }  
 }
 
 
 // Check if the Player's Guess is the winning number 
-var numberOfGuesses = 0;
-var guesses = [];
+//var numberOfGuesses = 0;
+//var guesses = [];
 
-function checkGuess(){ 
-    if (numberOfGuesses >= 4 && playersGuess !== winningNumber) {
-        numberOfGuesses++;
-        $('#guessanswer').css('visibility','visible');
-        $('#guessanswer').text("No Luck This Time. Play Again!");
-    }
-    else if(playersGuess === winningNumber) {
+
+Game.prototype.checkGuess = function() { 
+    if(this.playersGuess === this.winningNumber) {
         $('#guessanswer').css('visibility','visible');
         $('#guessanswer').text("You Win!");
         $('#trophy').css('visibility','visible');
         $('#trophy').effect('bounce', {times:3},500); 
-    } else if (guesses.indexOf(playersGuess)>=0){
+    } else if (this.guesses.indexOf(this.playersGuess)>=0){
         $('#guessanswer').css('visibility','visible');
         $('#guessanswer').text("You already guessed that number!");
     } else {
-        numberOfGuesses++;
-        guesses.push(playersGuess);
+        this.numberOfGuesses++;
+        this.guesses.push(this.playersGuess);
         $('#guessanswer').css('visibility','visible');
-        $('#guessanswer').text(guessMessage());
+        $('#guessanswer').text(this.guessMessage());
     }
 }
 
@@ -86,12 +89,13 @@ function guessesRemaining(){
 
 // Create a provide hint button that provides additional clues to the "Player"
 
-function provideHint(){
-    if (numberOfGuesses<3) {
+
+Game.prototype.provideHint = function() {
+    if (this.numberOfGuesses<4) {
         $('guessanswer').css('visibility','visible');
         $('#guessanswer').text("Guess some more before you get a hint");
     } else {
-        var message = 'The correct number is one of the following: ' + Math.floor(Math.random() * 100) + ", " + winningNumber + ", " + Math.floor(Math.random() * 100);
+        var message = 'The correct number is one of the following: ' + Math.floor(Math.random() * 100) + ", " + this.winningNumber + ", " + Math.floor(Math.random() * 100);
         $('guessanswer').css('visibility','visible');
         $('#guessanswer').text(message);
     }
@@ -99,10 +103,10 @@ function provideHint(){
 
 // Allow the "Player" to Play Again
 
-function playAgain(){
-    winningNumber = generateWinningNumber();
-    numberOfGuesses = 0;
-    guesses = [];
+Game.prototype.playAgain = function() {
+    this.winningNumber = this.winningNumberGenerator();
+    this.numberOfGuesses = 0;
+    this.guesses = [];
     $('#guessanswer').css('visibility','hidden');
     $('#trophy').css('visibility','hidden');
 }
@@ -110,27 +114,25 @@ function playAgain(){
 
 /* **** Event Listeners/Handlers ****  */
 $(document).ready(function() {
+    var newGame = new Game();
+    newGame.winningNumberGenerator();
     $('#hintbutton').on('click', function () {
-        provideHint();
+        newGame.provideHint();
     });
 
     $('#playagain').on('click', function () {
-        playAgain();
-        $('#guess-counter').html('<h3>'+guessesRemaining()+'</h3>');
+        newGame.playAgain();
     });
 
     $('#num-input').keypress(function(e) {
         if(e.which == 13) {
-            playersGuessSubmission();
-            checkGuess();
-            $('#guess-counter').html('<h3>'+guessesRemaining()+'</h3>');
+            newGame.playersGuessGenerator();
+            newGame.checkGuess();
         }
     });
 
     $('.main-box').on('click', 'button', function () {
-        playersGuessSubmission();
-        checkGuess();
-        $('#guess-counter').html('<h3>'+guessesRemaining()+'</h3>');
+        newGame.playersGuessGenerator();
+        newGame.checkGuess();
     });
-
 });
